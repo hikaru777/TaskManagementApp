c1a82e7b0d501fdc41e24dbc4d1a7c524fe7cf01
HikaruHonda <hikaru.h.developer@gmail.com>
2024-10-13 12:15:44 +0900

    Initial Commit

---
+++ KihomoriApp/KihomoriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KihomoriAppApp.swift
+//  KihomoriApp
+//
+//  Created by 本田輝 on 2024/10/13.
+//
+
+import SwiftUI
+
+@main
+struct KihomoriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KihomoriAppTests/KihomoriAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  KihomoriAppTests.swift
+//  KihomoriAppTests
+//
+//  Created by 本田輝 on 2024/10/13.
+//
+
+import Testing
+@testable import KihomoriApp
+
+struct KihomoriAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
